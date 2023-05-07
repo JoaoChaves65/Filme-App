4084155f4502c6495e3e2b766f78af2a93f141c7
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-05-07 10:20:56 -0300

    Pegando dados da API, exibindo títulos dos 20 melhores filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react"
+
+const filmesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 export const Home = () => {
+  const [topFilmes, setTopFilmes] = useState([])
+
+  const getTopRatedFilmes = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopFilmes(data.results);
+  };
+
+  useEffect (() => {
+    const topRatedURL = `${filmesURL}top_rated?${apiKey}`;
+
+    getTopRatedFilmes(topRatedURL);
+    
+  }, [])
+
   return (
-    <div>home</div>
+    <div>
+      {topFilmes && topFilmes.map((movie) => <p>{movie.title}</p>)}
+    </div>
   )
 }
